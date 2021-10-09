30bd0cb426d5f36593c331df1ff63d710f88e2ed
inoUwU <87593133+inoUwU@users.noreply.github.com>
2021-10-09 18:18:07 +0900

    add file

---
+++ Gb-keyframe.jsx
@@ -0,0 +1,163 @@
+/*
+Gb-Keyframe
+verjon 1.0
+*/
+var toolPanel = createUI(this);
+//---------scriptUI-------------------
+function createUI(thisObj) {
+  var scriptname = "Gb-Keyframe";
+  var myPanel =
+    thisObj instanceof Panel
+      ? thisObj
+      : new Window("palette", scriptname, undefined, { resizeable: true });
+  var tab = myPanel.add("tabbedpanel");
+  var tab1 = tab.add("tab", undefined, "Delete");
+  tab1.margins = 1;
+  var tab2 = tab.add("tab", undefined, "Move");
+  tab2.margins = 1;
+  var framework = tab1.add("group");
+  framework.orientation = "column";
+  framework.margins = 5;
+  framework.alignment = ["fill", "center"];
+  var optionsGrp = framework.add("panel", undefined, "Delete Setting");
+  optionsGrp.name = ["options"];
+  optionsGrp.margins = 10;
+  optionsGrp.orientation = "column";
+  optionsGrp.alignment = ["center", "center"];
+  var dualRadioBtn = optionsGrp.add(
+    "radiobutton",
+    [30, 10, 220, 30],
+    "Start and End Keyframe"
+  );
+  var startRadioBtn = optionsGrp.add(
+    "radiobutton",
+    [30, 10, 220, 30],
+    "StartPoint Keyframe"
+  );
+  var endRadioBtn = optionsGrp.add(
+    "radiobutton",
+    [30, 10, 220, 30],
+    "EndPoint Keyframe"
+  );
+  var runBtn = framework.add("button", undefined, "Execution");
+  runBtn.alignment = ["fill", "center"];
+  dualRadioBtn.value = true; //selected
+  //tab2
+  var framework2 = tab2.add("group");
+  framework2.orientation = "column";
+  framework2.margins = 5;
+  framework2.alignment = ["fill", "center"];
+  var optionsGrp2 = framework2.add("panel", undefined, "Move Settings");
+  optionsGrp2.name = ["options"];
+  optionsGrp2.margins = 10;
+  optionsGrp2.orientation = "column";
+  optionsGrp2.alignment = ["fill", "center"];
+  optionsGrp2.add(
+    "statictext",
+    [30, 10, 220, 30],
+    "※Move keyframe onto indicator"
+  );
+  var startKey = optionsGrp2.add(
+    "radiobutton",
+    [30, 10, 220, 30],
+    "StartPoint Keyframe"
+  );
+  var endKey = optionsGrp2.add(
+    "radiobutton",
+    [30, 10, 220, 30],
+    "EndPoint Keyframe"
+  );
+  endKey.value = true; //selected
+  var runBtn2 = framework2.add("button", undefined, "Execution");
+  runBtn2.alignment = ["fill", "center"];
+
+  //ButtonClickEvents
+  runBtn.onClick = function () {
+    var deleteMode = 0;
+    if (dualRadioBtn.value === true) {
+      deleteMode = 0;
+    } else if (startRadioBtn.value === true) {
+      deleteMode = 1;
+    } else if (endRadioBtn.value === true) {
+      deleteMode = 2;
+    }
+    DeleteExecution(deleteMode);
+  };
+  runBtn2.onClick = function () {
+    var moveMode = 0;
+    if (startKey.value === true) {
+      moveMode = 0;
+    } else if (endKey.value === true) {
+      moveMode = 1;
+    }
+    MoveExecution(moveMode);
+  };
+
+  return myPanel;
+}
+
+toolPanel.layout.layout();
+toolPanel.onResize = function () {
+  toolPanel.layout.resize();
+};
+if (toolPanel instanceof Window) {
+  toolPanel.center();
+  toolPanel.show();
+}
+//---------------functions-----------------------------
+function DeleteExecution(mode) {
+  var SelectedLayer = app.project.activeItem.selectedLayers;
+  if (SelectedLayer.length !== 0) {
+    for (var i = 0; i < SelectedLayer.length; i++) {
+      var isTImeRemap = SelectedLayer[i].canSetTimeRemapEnabled;
+      if (isTImeRemap === false) {
+        continue;
+      }
+      var timeRemapProp = SelectedLayer[i].property("ADBE Time Remapping");
+      var keysIndex = timeRemapProp.numKeys;
+      if (mode === 0) {
+        timeRemapProp.removeKey(keysIndex);
+        timeRemapProp.removeKey(1);
+      } else if (mode === 1) {
+        timeRemapProp.removeKey(1);
+      } else {
+        timeRemapProp.removeKey(keysIndex);
+      }
+      if (SelectedLayer.length === 1) {
+        timeRemapProp.selected = true;
+      }
+    }
+  } else {
+    alert("Layer is not selected");
+  }
+}
+
+function MoveExecution(mode) {
+  var time = [app.project.activeItem.time];
+  var SelectedLayer = app.project.activeItem.selectedLayers;
+  if (SelectedLayer.length !== 0) {
+    for (var i = 0; i < SelectedLayer.length; i++) {
+      var isTImeRemap = SelectedLayer[i].canSetTimeRemapEnabled;
+      if (isTImeRemap === false) {
+        continue;
+      }
+      var timeRemapProp = SelectedLayer[i].property("ADBE Time Remapping");
+      var keysIndex = timeRemapProp.numKeys;
+      var setVal = [];
+      if (mode === 0) {
+        setVal.push(timeRemapProp.keyValue(1));
+        timeRemapProp.removeKey(1);
+        timeRemapProp.setValuesAtTimes(time, setVal);
+      } else if (mode === 1) {
+        setVal.push(timeRemapProp.keyValue(keysIndex));
+        timeRemapProp.removeKey(keysIndex);
+        timeRemapProp.setValuesAtTimes(time, setVal);
+      }
+      if (SelectedLayer.length === 1) {
+        timeRemapProp.selected = true;
+      }
+    }
+  } else {
+    alert("Layer is not selected");
+  }
+}
---
+++ README.md
@@ -0,0 +1,25 @@
+# **_Gb-Keyframe_**
+
+##### _Adobe After Effects のタイムリマップの keyframe を削除・移動させるスクリプトです。_
+
+## --導入方法--
+
+**_スクリプトは[こちら](https://qiita.com/Qiita/items/c686397e4a0f4f11683d#links---%E3%83%AA%E3%83%B3%E3%82%AF)からダウンロード出来ます！_**
+
+..\Adobe\Adobe After Effects[version]\Support Files\Scripts\ScriptUI Panels または  
+..\Adobe\Adobe After Effects[version]\Support Files\Scripts に配置してください。
+
+**_※ScriptUI Panels に配置したほうが使い易いと思います。_**
+
+## --起動方法--
+
+- ScriptUI Panels に配置した場合はメニューバー/ビューから呼び出すことが出来ます。
+- Scripts に配置した場合はメニューバー/ファイル/スクリプトから呼び出すことが出来ます。
+
+## --使用方法--
+
+まずスクリプトを起動してください。  
+Delete のタブと Move のタブに分かれて機能が分かれています。  
+Delete ではタイムリマップの開始点と終点のキーフレームを削除することが出来ます。  
+Move ではタイムリマップの開始点と終点のキーフレームを移動させることが出来ます。  
+実行するには Execution を押下してください。
